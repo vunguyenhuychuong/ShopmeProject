037c67d198dd877a6aa107b0d7661ddb27221026
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-09-10 14:12:52 +0700

    func: code Sorting for User List upgrade

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/MvcConfig.java
@@ -19,5 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/" + dirName + "/**")
                 .addResourceLocations("file:/" + userPhotosPath + "/");
+
+
     }
 }
